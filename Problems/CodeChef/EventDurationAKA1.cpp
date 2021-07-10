8d3a2e6049dfb71a3152ab151ecec31f77ebf5fc
AshminJayson <50941574+AshminJayson@users.noreply.github.com>
2021-07-10 21:45:06 +0530

    Update EventDurationAKA1.cpp

--- Problems/CodeChef/EventDurationAKA1.cpp
+++ Problems/CodeChef/EventDurationAKA1.cpp
@@ -4,28 +4,23 @@ using namespace std;
  
 int main()
 {
-int n,k;
-cin>>n>>k;
+int n, k, maxtime = 0;
+cin >> n >> k;
 int time[n];
-int i=0;
-for(i=0;i<n;i++)
+for(int i = 0; i < n; i++)
+    cin >> time[i];
+
+sort(time, time + n);
+for(int i = 0; i < n - 1; i++)
 {
-    cin>>time[i];
+    if(k <= time[i + 1] - time[i])
+    maxTime += k;
+    else
+    maxTime += time[i + 1] - time[i];
 }
 
-int maxTime=0;
-
-sort(time,time + n);
-for(i=0;i<n-1;i++)
-{
-        if(k<=time[i+1]-time[i])
-        maxTime+=k;
-        else
-        maxTime+=time[i+1]-time[i];
-}
-
-maxTime+=k;
-cout<<maxTime<<"\n";
+maxTime += k;
+cout << maxTime << "\n";
 
 return 0;
 }

66e7d85dd56ca0597d142fd9a13d4dd5bbca1205
AshminJayson <50941574+AshminJayson@users.noreply.github.com>
2021-07-31 22:29:16 +0530

    Update HELPAKA2 .cpp

--- Problems/CodeChef/HELPAKA2 .cpp
+++ Problems/CodeChef/HELPAKA2 .cpp
@@ -2,28 +2,26 @@
  
 using namespace std;
 
-long maxpossible(long diff[],long i,long n,long k, long x)
+long maxpossible(long diff[], long i, long n, long k, long x)
 {
-    if(i>=n)
+    if(i >= n)
     return 0;
 
-    long r=0;
-    if(diff[i]<diff[i-1])
-    r=1+maxpossible(diff,i+1,n,k,x);
+    long r = 0;
+    if(diff[i] < diff[i-1])
+    r = 1 + maxpossible(diff, i + 1, n, k, x);
     else
     {
-
-        
         long a,b;
-        a=b=-1;
-        if(k>=(diff[i]-diff[i-1]))
-        a=1+maxpossible(diff,i+1,n,k-(diff[i]-diff[i-1]),x);
+        a = b = -1;
+        if(k >= (diff[i] - diff[i-1]))
+        a=1+maxpossible(diff, i + 1, n, k - (diff[i] - diff[i - 1]), x);
         
-        if(x!=0)
-        b=1+maxpossible(diff,i+1,n,k,x-1);
+        if(x != 0)
+        b = 1 + maxpossible(diff, i + 1, n, k, x - 1);
         
         
-        r=max(a,b);
+        r = max(a, b);
 
         if(r == -1)
         return 0;
@@ -36,17 +34,14 @@ long maxpossible(long diff[],long i,long n,long k, long x)
 
 int main()
 {
-long n,k,x;
-cin>>n>>k>>x;
+long n, k, x;
+cin >> n >> k >> x;
 long diff[n];
 
-for(long i=0;i<n;i++)
-{
-    cin>>diff[i];
-}
+for(long i = 0; i < n; i++)
+    cin >> diff[i];
 
-
-cout<<maxpossible(diff,1,n,k,x);
+cout<<maxpossible(diff, 1, n, k, x);
 
 return 0;
 }

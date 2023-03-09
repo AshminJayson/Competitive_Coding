16da55da00a2157fa483a6b465a5a11f6681608e
Ashmin Jayson <ashminjayson10@gmail.com>
2023-03-09 19:48:16 +0530

    Day 32: A bad word and a Fast Hare

---
+++ LeetCode Problems/212. Word Search II.py
@@ -0,0 +1,70 @@
+#I am not very happy with this, I had the answer but didn't think it was right and didn't go with the solution
+
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+        self.ref = 0
+
+    def addword(self, word):
+        curr = self
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Trie()
+            curr = curr.children[c]
+            curr.ref += 1
+        curr.end = True
+
+    def removeword(self, word):
+        curr = self
+        curr.ref -= 1
+
+        for c in word:
+            if c in curr.children:
+                curr = curr.children[c]
+                curr.ref -= 1
+
+
+            
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+
+        root = Trie()
+        for word in words:
+            root.addword(word)
+
+        R, C = len(board), len(board[0])
+        valid, visited = set(), set()
+
+        def dfs(r, c, node, word):
+
+            if ( r < 0 or r >= R or c < 0 or c >= C or (r, c) in visited
+            or board[r][c] not in node.children or 
+            node.children[board[r][c]].ref < 1 ):
+                return
+
+            visited.add((r,c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+
+            if node.end:
+                node.end = False
+                valid.add(word)
+                root.removeword(word)
+
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r , c + 1, node, word)
+            dfs(r, c - 1, node, word)
+
+            visited.remove((r, c))
+        
+        for i in range(R):
+            for j in range(C):
+                dfs(i, j, root, "")
+
+        return list(valid)
+
+
+        

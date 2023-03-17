c43b3bbdeed5100a1e02033f563687c21dbb0baa
Ashmin Jayson <ashminjayson10@gmail.com>
2023-03-17 19:32:16 +0530

    Day 35 Update : Added heap solution to Find Median From Data Stream

--- LeetCode Problems/295. Find Median from Data Stream.py
+++ LeetCode Problems/295. Find Median from Data Stream.py
@@ -42,3 +42,52 @@ class MedianFinder:
 
 #Heap Based Approach
 
+from heapq import heappop, heappush
+
+class MedianFinder:
+
+    def __init__(self):
+        self.sm = []
+        self.bm = []
+        self.n = 0
+        
+
+    def addNum(self, num: int) -> None:
+        self.n += 1
+
+        if len(self.sm) == len(self.bm):
+            if self.sm and -self.sm[0] >= num:
+                heappush(self.sm, -num)
+            else:
+                heappush(self.bm, num)
+        elif len(self.sm) > len(self.bm):
+            if self.sm and -self.sm[0] >= num:
+                x = heappop(self.sm)
+                heappush(self.sm, -num)
+                heappush(self.bm, -x)
+            else:
+                heappush(self.bm, num)
+        else:
+            if self.bm and self.bm[0] < num:
+                x = heappop(self.bm)
+                heappush(self.bm, num)
+                heappush(self.sm, -x)
+            else:
+                heappush(self.sm, -num)
+
+        # print(self.sm, self.bm)
+
+    def findMedian(self) -> float:
+        if self.n % 2 == 0:
+            return (-self.sm[0] + self.bm[0]) / 2
+        else:
+            if len(self.sm) > len(self.bm):
+                return -self.sm[0]
+            else:
+                return self.bm[0]
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()

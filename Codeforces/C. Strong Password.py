35a161ab9fe01afad7b1af6157e4bed80b37175d
Ashmin Jayson <ashminjayson10@gmail.com>
2023-07-01 00:36:46 +0530

    ECR 151: Password Problem

--- Codeforces/C. Strong Password.py
+++ Codeforces/C. Strong Password.py
@@ -1,5 +1,3 @@
-# This solution results in TLE
-
 t = int(input())
 
 def solution():
@@ -8,41 +6,38 @@ def solution():
 	l = input()
 	r = input()
 
-	prev = []
+	dp = {}
+	for i in range(len(s)):
+		dp.setdefault(s[i], []).append(i)
 
-	def issub(i, j, strb):
-		i, j = 0, 0
+	cur = 0
 
-		while i < len(strb) and j < len(s):
-			if strb[i] == s[j]:
-				i += 1
-				j += 1
-			else:
-				j += 1
+	for i in range(m):
+		start = int(l[i])
+		end = int(r[i]) + 1
+		mx = cur + 0
 
-		if i == len(strb): return True
-		if j == len(s): return False
+		for j in range(start, end):
 
+			if str(j) not in dp:
+				mx = len(s)
+				break
 
+			def getNext(l, cr):
+				for i in l:
+					if i >= cr:
+						return i
 
-	def dfs(i, token):
-		if i == m:
-			if not issub(0, 0, token):
-				return True
-			return False
+				return len(s)
 
-		for k in range(int(l[i]), int(r[i]) + 1):
-			token.append(str(k))
-			if dfs(i+1, token):
-				return True
-			token.pop()
+			mx = max(mx,  getNext(dp[str(j)], cur))
+
+		cur = mx + 1
 
 
-	if dfs(0, []):
-		print("YES")
-	else:
-		print("NO")
+	print("YES" if cur > len(s) else "NO")
 	return
+
 
 for _ in range(t):
 	solution()

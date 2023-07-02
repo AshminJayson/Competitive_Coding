d92fdf08c066b8e4bda7a6e027844a55bd17c3f9
Ashmin Jayson <ashminjayson10@gmail.com>
2023-07-03 00:24:59 +0530

    Day 64: LC Daily July 2

---
+++ LeetCode Problems/1601. Maximum Number of Achievable Transfer Requests.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+
+        def dp(i, ar, rc):
+            if i == len(requests):
+                if max(ar) == 0:
+                    return rc
+                else:
+                    return 0
+
+            temp = ar.copy()
+            frm, to = requests[i]
+            temp[frm] -=1
+            temp[to] +=1 
+
+            return max(dp(i+1, temp, rc + 1), dp(i+1, ar, rc))
+
+        return dp(0, [0] * n, 0)

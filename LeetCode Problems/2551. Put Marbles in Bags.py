05879bc7d292e909fb15ea739d525e71b44d0d59
Ashmin Jayson <ashminjayson10@gmail.com>
2023-07-08 17:50:15 +0530

    Day 70: Heap finally helped

---
+++ LeetCode Problems/2551. Put Marbles in Bags.py
@@ -0,0 +1,28 @@
+from heapq import heapify, heappop
+
+class Solution:
+    def putMarbles(self, weights: list[int], k: int) -> int:
+        
+        mx, mn = [0] * (len(weights) - 1), [0] * (len(weights) - 1)
+
+        for i in range(len(weights) - 1):
+        	loss = (weights[i] * 2  + weights[i + 1] * 2) - (weights[i] + weights[i + 1])
+        	mx[i], mn[i] = loss, -loss
+
+        heapify(mx)
+        heapify(mn)
+
+
+        maxScore, minScore = sum(weights) * 2, sum(weights) * 2
+
+        for i in range(len(weights) - k):
+        	maxScore -= heappop(mx)
+        	minScore -= -heappop(mn)
+
+        return maxScore - minScore
+
+
+
+ob = Solution()
+print(ob.putMarbles([1,3,5,1], 2))
+print(ob.putMarbles([1,3], 2))
